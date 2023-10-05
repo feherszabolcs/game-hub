5a01b9ea2844c6d5211c11d5d5d8221a6d3aa5ef
feherszabolcs <feherszabi2005@gmail.com>
2023-10-05 12:53:34 +0200

    GameCards built

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
     id: number,
-    name: string
+    name: string,
+    background_image : string,
 }
 
 interface FetchGames {
@@ -25,7 +26,7 @@ const useGames = () =>{
                 if(err instanceof CanceledError) return;
                 setError(err.message) 
             })
-            return controller.abort();
+     return ()=> controller.abort();
     },[])
 
     return {games, error}
