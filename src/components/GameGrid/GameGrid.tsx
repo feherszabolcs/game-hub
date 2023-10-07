2a17244cdadb4087a46dc977038927032a2e4248
feherszabolcs <feherszabi2005@gmail.com>
2023-10-07 09:07:16 +0200

    loading skeletons

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -1,16 +1,20 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../../hooks/useGames';
 import GameCard from '../GameCard/GameCard';
+import SkeletonCard from '../GameCard/SkeletonCard';
 
 
 
 const GameGrid = () => {
-    const { error, games } = useGames();
+    const { error, games, isLoading } = useGames();
+
+    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     return (
         <>
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding={10} spacing={10} >
                 {games.map(game => (<GameCard key={game.id} game={game} />))}
+                {isLoading && skeletons.map(s => <SkeletonCard key={s} />)}
             </SimpleGrid>
         </>
     )
