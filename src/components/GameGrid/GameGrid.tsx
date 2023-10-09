f47032178ca7c4d8a723c9515a9af89df081eca7
feherszabolcs <feherszabi2005@gmail.com>
2023-10-09 09:36:57 +0200

    data fetching hook created

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -7,14 +7,14 @@ import CardContainer from '../GameCard/CardContainer';
 
 
 const GameGrid = () => {
-    const { error, games, isLoading } = useGames();
+    const { error, data, isLoading } = useGames();
 
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     return (
         <>
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding={10} spacing={10} >
-                {games.map(game => (
+                {data.map(game => (
                     <CardContainer>
                         <GameCard key={game.id} game={game} />
                     </CardContainer>))}
